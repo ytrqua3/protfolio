cb3587779f08ca9a80a948c4e75049189e01ed1a
ytrqua3 <suensenho@gmail.com>
2025-03-03 12:41:21 -0800

    added thumbnail images for side projects and data for current side projects

---
+++ scripts/data/side_projects.js
@@ -0,0 +1,39 @@
+const side_projects = [
+    {
+        "id": '111',
+        "name": 'Trombone product page',
+        "img": 'assets/images/thumbnail-111.png',
+        "description": 'frontend webpage built using basic html, css in reference to a freecodecamp project',
+        "finished": true,
+        "date": '2024-08-22T00:00:00-08:00',
+        "keywords": ['html', 'css', 'product', 'e-commerece'],
+    },
+    {
+        "id": '112',
+        "name": 'calory calculator',
+        "img": 'assets/images/thumbnail-112.png',
+        "description": 'frontend webpage built in reference to a freecodecamp project. Built to learn basic interactive JS.',
+        "finished": true,
+        "date": '2025-02-21T00:00:00-08:00',
+        "keywords": ['html', 'css', 'interactive', 'calculator', 'js', 'javascript'],
+    },
+    {
+        "id": '113',
+        "name": 'contacts page template',
+        "img": 'assets/images/thumbnail-113.png',
+        "description": 'interactive contact form using Web3Forms api to send message via email. Built for preparation for portfolio',
+        "finished": true,
+        "date": '2025-03-01T00:00:00-08:00',
+        "keywords": ['html', 'css', 'interactive', 'form', 'js', 'javascript', 'contact', 'leave', 'a', 'message', 'api', 'email'],
+    },
+    {
+        "id": '114',
+        "name": 'nutrition tag',
+        "img": 'assets/images/thumbnail-114.png',
+        "description": 'nutrition tag built in reference to freecodecamp, used to learn flex styling',
+        "finished": true,
+        "date": '2024-09-03T00:00:00-08:00',
+        "keywords": ['html', 'css', 'flex', 'styling'],
+    },
+
+]
